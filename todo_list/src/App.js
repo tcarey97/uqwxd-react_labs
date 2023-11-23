5808fec06401b86df7d0121bcfb4b2cd20edb8d2
tcarey97 <125526091+tcarey97@users.noreply.github.com>
2023-11-23 15:12:39 +1100

    Update App.js

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,28 +1,64 @@
-import React, {useState} from "react";
+import React,{useState} from "react";
 import "./App.css";
+
 const App = () => {
   const [todos, setTodos] = useState([]);
-  
-  // Add the handlesubmit code here
-  
-  
-  // Add the deleteToDo code here
 
-  
-  // Add the toggleComplete code here
+  function handleSubmit(e) {
+    e.preventDefault();
 
-  
-  // Add the submitEdits code here
+    let todo = document.getElementById('todoAdd').value
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo.trim(),
+      completed: false,
+    };
+    if (newTodo.text.length > 0 ) {
+        setTodos([...todos].concat(newTodo));
+    } else {
 
-  
-return(
-<div className ="App">
-<h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" id= 'todoAdd'/>
-<button type ="submit">Add Todo</button>
-</form>
-</div>
-);
+        alert("Enter Valid Task");
+    }
+    document.getElementById('todoAdd').value = ""
+  }
+
+  function deleteToDo(id) {
+      let updatedTodos = [...todos].filter((todo) => !todo.id);
+      setTodos(updatedTodos);
+  }
+
+  function toggleComplete(id) {
+      let updatedTodos = [...todos].map((todo) => {
+          if (todo.id === id){
+              todo.completed = !todo.completed;
+          }
+
+          return todo;
+      })
+
+      setTodos(updatedTodos);
+
+  }
+
+    return (
+      <div id="todo-list">
+        <h1>Todo List</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              id = 'todoAdd'
+            />
+            <button type="submit">Add Todo</button>
+        </form>
+        {todos.map((todo) => <div className="todo" key={todo.id}>
+            <div className="todo-text">{todo.text}
+            <input type="checkbox" id="completed" checked={todo.completed} onChange={() => toggleComplete(todo.id)}></input>
+            </div>
+            <button onClick={() => deleteToDo(todo.id)}>Delete</button>
+
+        </div>)}
+        </div>
+  );
 };
+
 export default App;
